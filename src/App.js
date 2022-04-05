7feabe55f92dfd7d02c5d35a02cd8e3df49bb18a
muzahid604 <604muzahid@gmail.com>
2022-04-05 11:16:48 +0600

    route notfound added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Blogs from './components/Blogs/Blogs';
 import Dashbord from './components/Dashbord/Dashbord';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashbord' element={<Dashbord></Dashbord>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
 
     </div>
